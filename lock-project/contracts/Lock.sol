d217fe4732e1595fded0c93a7cb7e25cff8da38c
Cetin Guven <117115603+CetinGuven@users.noreply.github.com>
2023-01-30 10:57:48 +0300

    lock-project

---
+++ lock-project/contracts/Lock.sol
@@ -0,0 +1,54 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+import "./Token.sol";
+
+contract Lock {
+    BEEToken Token;
+    uint256 public lockerCount;
+    uint256 public totalLocked;
+    mapping(address => uint256) public lockers;
+    mapping(address => uint256) public deadline;
+ 
+    constructor(address tokenAddress) {
+        Token = BEEToken(tokenAddress);
+    }
+
+    function lockTokens(uint256 amount, uint256 time) external{ 
+        require(amount > 0, "Token amount must be bigger than 0.");
+        require(time > 0, "Locking time must be bigger than 0.");
+        
+        if(!(lockers[msg.sender] > 0))lockerCount++;
+        totalLocked+=amount;
+        lockers[msg.sender]+=amount;
+        deadline[msg.sender] = block.timestamp + time;
+
+        bool ok =Token.transferFrom(msg.sender, address(this), amount);
+        require(ok, "Transfer failed.");
+    }
+
+    function withdrawTokens() external {
+        require(lockers[msg.sender] > 0, "You don't have any tokens locked.");
+        require(block.timestamp >= deadline[msg.sender], "Deadline is not over.");
+        uint256 amount = lockers[msg.sender];
+        delete(lockers[msg.sender]);
+        delete(deadline[msg.sender]);
+        totalLocked -= amount;
+        lockerCount--;
+
+
+
+
+        require(Token.transfer(msg.sender, amount),"Transfer failed.");
+
+
+    }
+ 
+   
+
+
+
+
+
+}
